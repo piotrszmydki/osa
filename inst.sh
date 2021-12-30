381105255bbc091fc2de500423aea61105bf0863
piotrszmydki <96536702+piotrszmydki@users.noreply.github.com>
2021-12-30 14:36:53 +0100

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 clear
-git clone https://github.com/piotrszmydki/piotrszmydki /etc/openstack_deploy
+git clone https://github.com/piotrszmydki/osa /etc/openstack_deploy
 git clone -b master https://opendev.org/openstack/openstack-ansible /opt/openstack-ansible
 cd /opt/openstack-ansible
 scripts/bootstrap-ansible.sh
 cp -R /opt/openstack-ansible/etc/openstack_deploy /etc/
+echo " copy create disks script for swift to host 172.29.236.211"
 scp /etc/openstack_deploy/make-swift-disk.sh root@172.29.236.211:/root/
+echo " copy create disks script for lvm to host 172.29.236.213"
 scp /etc/openstack_deploy/make-lvm-disk.sh root@172.29.236.213:/root/
 #cp /etc/openstack_deploy/openstack_user_config.yml.example /etc/openstack_deploy/openstack_user_config.yml
 cd /opt/openstack-ansible
@@ -19,5 +21,9 @@ nano /opt/openstack-ansible/inventory/group_vars/ceph_all.yml
 echo " Change dalay in section install distro package from 2 to 5. #Because you will see error: connection failed"
 sleep 5
 nano /opt/openstack-ansible/playbooks/utility-install.yml
+echo " add venv_wheel_build_skip_check: true to /etc/openstack_deploy/user_variables.yml"
+sleep 5
+nano /etc/openstack_deploy/user_variables.yml
 cd /opt/openstack-ansible/playbooks
-
+echo " END"
+sleep 5
